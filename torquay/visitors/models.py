9bdd2d22ef084fa331b372cd5eb6f604bec5b91e
MiDev7 <razafintsalamamikaia@gmail.com>
2022-09-16 16:01:12 +0400

    Models and test setup

--- torquay/visitors/models.py
+++ torquay/visitors/models.py
@@ -1,5 +1,8 @@
+from tabnanny import check
+from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 # Create your models here.
@@ -13,21 +16,73 @@ class Feature(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=250)
     features = models.ManyToManyField(Feature)
+    max_people = models.IntegerField( default= 1,null=True)
+    number_of_beds = models.IntegerField(default= 1,null=True)
+    price = models.IntegerField(default= 500,null=True)
+    pet_allowed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f" {self.name }   {self.price}"   
 
     
 class Room(models.Model):
     availability = models.BooleanField(default=True)
-    max_people = models.IntegerField(max_length=20)
-    number_of_beds = models.IntegerField()
-    price = models.IntegerField(max_length=500)
-    pet_allowed = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL , null=True)
 
+    def __str__(self):
+        return f"{self.id}  {self.category} "
+
+    @classmethod
+    def search(cls,date_in,date_out,category_id):
+        available_room = []
+        category = Category.objects.get(id=category_id)
+        rooms = Room.objects.filter(category=category)
+
+        for room in rooms:
+            booking = Booking.objects.filter(
+                models.Q(room=room) & (
+                models.Q(check_in_date__lte=date_in) & models.Q(check_out_date__gte=date_in) |
+                models.Q(check_in_date__lte=date_out) & models.Q(check_out_date__gte=date_out)
+                
+            ))
+            if (len(booking) == 0):
+                available_room.append(room)
+
+        return available_room
+
+    def book(self,date_in,date_out,user,number_of_people):
+        booked =  Booking(
+            user = user,
+            room = self,
+            check_in_date = date_in,
+            check_out_date = date_out,
+            number_of_people = number_of_people
+        )
+
+        booked.save()
+        return booked
+
+    @property
+    def total_price(self,date_in,date_out):
+        time_stay = date_out - date_in
+        total = time_stay * self.category.price
+        return total 
+
+
 class Booking(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE , null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE , null=True)
     room = models.ForeignKey(Room,on_delete=models.SET_NULL , null=True)
     check_in_date = models.DateField()
     check_out_date = models.DateField()
     confirmed = models.BooleanField(default=False)
-    number_of_people = models.IntegerField(max_length=15, blank=False,null=True)
+    number_of_people = models.IntegerField(blank=False,null=True)
+    price = models.IntegerField(null=True)
+    
+    def save(self, *args, **kwargs):
+        date_format = "%Y-%m-%d"
+        delta = (datetime.strptime(self.check_out_date, date_format) - datetime.strptime(self.check_in_date, date_format)) 
+
+        self.price = delta.days * self.room.category.price
+        super(Booking, self).save(*args, **kwargs)
+
 
--- torquay/visitors/tests.py
+++ torquay/visitors/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+from .models import *
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class BookingTestCase(TestCase):
+    
+    fixtures =  ["features.json","category.json","room.json"]
+
+    def setUp(self):
+        myuser = User(username="Tony",password="lol77")
+        myuser.save()
+    def test_room_availability(self):
+        rooms = Room.search("2022-08-12","2022-09-10",3)
+        self.assertEqual(len(rooms),10)  
+        name = User.objects.get(username= "Tony")
+        r1 = rooms[1].book("2022-08-12","2022-09-10",name,2)
+
+        self.assertEqual(r1.price,9)
+        r2= Room.search("2022-08-12","2022-09-10",3)
+        self.assertEqual(len(r2),9)
+
+
