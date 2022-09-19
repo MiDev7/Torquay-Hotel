395206c1e620550a4576b81bb436fd45ed8713c8
MiDev7 <razafintsalamamikaia@gmail.com>
2022-09-19 10:19:58 +0400

    Login form and Signup form

--- torquay/visitors/urls.py
+++ torquay/visitors/urls.py
@@ -6,4 +6,7 @@ from .views import *
 urlpatterns = [
     path('booking/',booking,name="booking" ),
     path('info/',info,name="info" ),
+    path('signup/', signup_view, name='signup'),
+    path('login/',login_view, name='login'),
+    path('logout/',logout_view, name='logout'),
 ]
