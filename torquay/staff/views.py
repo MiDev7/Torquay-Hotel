f20bac16cf2b314a7e6af345a7f279ee1be5877d
Vidhipriya <lvidhipriyapokiril@gmail.com>
2022-09-20 10:23:39 +0400

    changed calendar

--- torquay/staff/views.py
+++ torquay/staff/views.py
@@ -1,21 +1,16 @@
 
 from django.shortcuts import render
 from django.http import JsonResponse
+from visitors.models import *
 # Create your views here.
 def getbookings(request):
-    bookings=[
-    {
-      'id': 'a',
-      'title': 'my event',
-      'start': '2022-09-16'
-    }
-    ,{
-      'id': 'b',
-      'title': 'second',
-      'start': '2022-09-20'
-    }
-  ]
-    return JsonResponse(bookings,safe=False)
+    booking_array=[]
+    bookings=Booking.objects.all()
+  
+    for i in bookings:
+      i={'id':i.id,'title':f"{i.user.username} {i.room.category.name}",'start':i.check_in_date,'end':i.check_out_date}
+      booking_array.append(i)
+    return JsonResponse(booking_array,safe=False)
 
 def showcalender(request):
     return render(request,'base2.html',context={})
